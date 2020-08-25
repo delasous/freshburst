ff1a5b879572c3351b475a736f2252d10bdeaaa0
Work <sousa.yak@googlemail.com>
2020-08-25 09:23:47 +0200

    adjusted logo sizing & overlap

--- components/Logo.js
+++ components/Logo.js
@@ -1,21 +1,36 @@
 export default function Logo () {
   return (
   <>
-    <div className='logo-container'>
-      <img src='/explode-crop-1.gif' alt='exploding burst' className='logo'/>
+    <div className='container'>
+      <div className='logo-container'>
+        <img src='/explode-crop-1.gif' alt='exploding burst' className='logo'/>
+      </div>
+      <h1>fresh burst</h1>
     </div>
     <style jsx>{`
-    .logo {
+    .logo-container {
       display: block;
-      width: auto;
-      height: auto;
+      width: 110px;
+      height: 110px;
     }
 
-    .logo-container {
+    .logo {
+      max-width: 150px;
+      max-height: 150px
+    }
+
+    .container {
       display: flex;
-      grid-row: 1;
       justify-content: center;
+      grid-row: 1;
       grid-column: 1 / 1;
+    }
+
+    .container h1 {
+      font-weight: 400;
+      color: #212121;
+      font-size: 2.5em;
+      margin: 10px 0 10px 5px;
     }
 
   `}</style>
