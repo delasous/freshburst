d26bd6f08bd7c23779ab2bf64f783945f98652d4
Work <sousa.yak@googlemail.com>
2020-09-05 19:22:53 +0200

    adjustments to Logo tailwind layout

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,12 @@
 export default function Logo () {
   return (
-    <div class='flex flex-wrap justify-center grid-row-span-1 grid-col-span-1'>
-      <div class='block w-110px h-110px'>
-        <img class='w-150px h-150px' src='/explode-crop-1.gif' alt='exploding burst' />
+    <div class='flex justify-center row-span-1 col-span-1'>
+      <div class='block w-110 h-110'>
+        <img class='w-150 h-150' src='/explode-crop-1.gif' alt='exploding burst' />
       </div>
-      <h1 class='font-normal text-4xl text-secondary my-10 mr-5'>fresh burst</h1>
+      <div class='block w-110 h-110'>
+        <h1 class='font-normal text-4xl text-secondary -ml-5 my-2'>fresh burst</h1>
+      </div>
     </div>
   )
 }
