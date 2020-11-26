73bf9b5fb56e32a2855f66b88ca722365470e9c2
Work <sousa.yak@googlemail.com>
2020-11-26 22:14:14 +0100

    fixed site title - inline-block to float

--- components/Logo.js
+++ components/Logo.js
@@ -4,9 +4,9 @@ export default function Logo () {
   return (
       <div className='w-40 max-h-24'>
         <Link href='/'>
-        <h1 className='p-10 font-bold text-4xl text-center xsm:inline-block xsm:bg-cliptext-swirl'>
-          <span className='w-auto cursor-pointer xsm:inline-block md:bg-cliptext-swirl hover:text-secondary '> fresh</span>
-          <span className='w-auto cursor-pointer xsm:inline-block md:bg-cliptext-swirl hover:text-secondary '>burst</span>
+        <h1 className='p-10 font-bold text-4xl text-center leading-10 cursor-pointer xsm:inline-block xsm:bg-cliptext-swirl'>
+          <span className='w-auto xsm:float-left md:bg-cliptext-swirl hover:text-secondary'> fresh</span>
+          <span className='w-auto xsm:clear-left md:bg-cliptext-swirl hover:text-secondary'>burst</span>
         </h1>
         </Link>
       </div>
