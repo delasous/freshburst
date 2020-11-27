189b669573e46e62facddd6055d4300325fc384e
Work <sousa.yak@googlemail.com>
2020-11-27 09:11:40 +0100

    site title span grid to fix float collapse

--- components/Logo.js
+++ components/Logo.js
@@ -4,11 +4,13 @@ export default function Logo () {
   return (
       <div className='w-40 max-h-24'>
         <Link href='/'>
-        <h1 className='p-10 font-bold text-4xl text-center leading-10 cursor-pointer xsm:inline-block xsm:bg-cliptext-swirl'>
-          <span className='w-auto xsm:float-left md:bg-cliptext-swirl hover:text-secondary'> fresh</span>
-          <span className='w-auto xsm:clear-left md:bg-cliptext-swirl hover:text-secondary'>burst</span>
+        <h1 className='p-10 font-bold text-4xl text-center leading-10 cursor-pointer xsm:grid xsm:grid-rows-2'>
+          <span className='bg-cliptext-swirl hover:text-secondary md:py-10'> fresh</span>
+          <span className='bg-cliptext-swirl hover:text-secondary md:py-10'>burst</span>
         </h1>
         </Link>
       </div>
   )
 }
+
+//
